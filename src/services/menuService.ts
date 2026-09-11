3e4df8b4b0c466819605ffc134ab93d2acd124b6
mahmoud2415 <01020805451oo@gmail.com>
2026-09-11 18:36:29 +0300

    feat: add high-res crispy chicken images, update crunchy menu items and bump cache version

--- src/services/menuService.ts
+++ src/services/menuService.ts
@@ -12,8 +12,8 @@ import { MenuItem, Deal } from '../types';
 import { initialMenuItems } from '../data/initialMenu';
 import { initialDeals } from '../data/dealsData';
 
-const MENU_STORAGE_KEY = 'zinger_local_menu_items_v7';
-const DEALS_STORAGE_KEY = 'zinger_local_deals_v7';
+const MENU_STORAGE_KEY = 'zinger_local_menu_items_v8';
+const DEALS_STORAGE_KEY = 'zinger_local_deals_v8';
 const FIREBASE_OPERATION_TIMEOUT = 30000;
 
 function removeUndefined<T extends object>(value: T): Partial<T> {
